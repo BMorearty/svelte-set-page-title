d81aa37bacdbac35cfa5aac7957de4488ce1dc4a
Brian Morearty <brian@morearty.org>
2023-01-20 11:35:43 -0800

    Set layout headline in page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { setContext } from 'svelte';
+  import { writable } from 'svelte/store';
+
+  let headline = setContext('headline', writable<string | undefined>(undefined));
+</script>
+
+<svelte:head>
+  <title>{$headline ? `${$headline} - ` : ''}Course Store</title>
+</svelte:head>
+
+{#if $headline}
+  <header>
+    <h1>{$headline}</h1>
+  </header>
+{/if}
+
+<main><slot /></main>
